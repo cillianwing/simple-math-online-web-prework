0abd69a7274c3579200f6b26407c0beb7206c271
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-19 12:35:26 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,35 @@
 def addition(num1, num2)
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
+  total = num1 - num2
+  total
 end
 
 def division(num1, num2)
+  total = num1 / num2
+  total
 end
 
 def multiplication(num1, num2)
+  total = num1 * num2
+  total
 end
 
 def modulo(num1, num2)
+  remainder = num1 % num2
+  remainder
 end
 
 def square_root(num)
+  value = Math.sqrt(num)
+  value
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  total = num1 + ((num2 * num3) / num4)
+  total
 end
